4d6ee1d35f281420593916f081226113e0edb5db
Junior Alves <jrnalves@gmail.com>
2024-08-19 17:26:04 -0300

    addicionado as rotas dinamicas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import Editor from "./components/Editor"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { v4 as uuidv4 } from "uuid"
 
 function App() {
   return (
-    <Editor />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
+        <Route path="/documents/:id" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
