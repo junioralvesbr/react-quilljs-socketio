2305c5c4d39170cf8ead2db65ac9ac916a2e8af5
Junior Alves <jrnalves@gmail.com>
2024-08-19 15:32:05 -0300

    add quilljs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,24 @@
+import { useCallback } from "react"
+import Quill from "quill"
+
+import "quill/dist/quill.snow.css";
+import './App.css'
+
 function App() {
+  const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
+    if (wrapper == null) return
+
+    wrapper.innerHTML = ""
+    const editor = document.createElement("div")
+    wrapper.append(editor)
+
+    new Quill(editor, { theme: 'snow' })
+
+  }, [])
+
+
   return (
-    <h1>Hello world</h1>
+    <div className="container" ref={wrapperRef}></div>
   )
 }
 
