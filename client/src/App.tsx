1ea275b914cb42db275698a75778ad67da964aaa
Junior Alves <jrnalves@gmail.com>
2024-08-19 15:34:48 -0300

    add toolbar config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,21 @@ import Quill from "quill"
 import "quill/dist/quill.snow.css";
 import './App.css'
 
+const toolBarsOptions = [
+  [{ 'font': [] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ size: ["small", false, "large", "huge"] }],
+  ["bold", "italic", "underline", "strike"],
+  [{ align: [] }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ direction: "rtl" }],
+  ["image", "link", "video", "blockquote", "code-block"],
+  ["clean"],
+]
+
 function App() {
   const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
     if (wrapper == null) return
@@ -12,7 +27,7 @@ function App() {
     const editor = document.createElement("div")
     wrapper.append(editor)
 
-    new Quill(editor, { theme: 'snow' })
+    new Quill(editor, { theme: 'snow', modules: { toolbar: toolBarsOptions } })
 
   }, [])
 
